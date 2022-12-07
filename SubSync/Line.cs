ca86f1f6ee7ef5ddd932147d139689180ed4a063
Ahmad Mutawa <ahmad.mutawa@gmail.com>
2022-12-07 17:55:33 +0300

    first commit

---
+++ SubSync/Line.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubSync
+{
+    public class Line
+    {
+        
+        
+
+        public Line(string sequence, DateTime startTime, DateTime endTime, string text)
+        {
+            Sequence = int.Parse(sequence);
+            StartTime = startTime;
+            EndTime = endTime;
+            Duration = (int)(EndTime - StartTime).TotalMilliseconds;
+            Text = text.Trim();
+        }
+
+        public int Sequence { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Text { get; set; }
+        public int Duration { get; set; }
+        public string Timing => $"{StartTime:HH:mm:ss,fff} --> {EndTime:HH:mm:ss,fff}";
+        public override string ToString()
+        {
+            return $"{StartTime:HH:mm:ss,fff} --> {EndTime:HH:mm:ss,fff}\r\n{Text}";
+        }
+
+        public void Shift(int milliseconds)
+        {
+            StartTime = StartTime.AddMilliseconds(milliseconds);
+            EndTime = EndTime.AddMilliseconds(milliseconds);
+        }
+
+        public void Sync(DateTime start1, DateTime stop1, DateTime start2, DateTime stop2)
+        {
+            StartTime = Map(StartTime, start1, stop1, start2, stop2);
+            EndTime = StartTime.AddMilliseconds(Duration);
+
+        }
+
+        
+        private static readonly DateTime origin = "00:00:00,000".ToDateTime();
+        public static DateTime Map(DateTime n, DateTime start1, DateTime stop1, DateTime start2, DateTime stop2)
+        {
+            double d = (n - start1).TotalMilliseconds;
+            double total1 = (stop1 - start1).TotalMilliseconds;
+            double total2 = (stop2 - start2).TotalMilliseconds;
+            double s2 = (start2 - origin).TotalMilliseconds;
+            double r = d / total1 * total2 + s2;
+
+            return origin.AddMilliseconds(r);
+        }
+    }
+
+    public static class Utils
+    {
+        public static void Shift(this IEnumerable<Line> lines, int milliseconds)
+        {
+            foreach(Line line in lines)
+            {
+                line.Shift(milliseconds);
+            }
+        }
+        public static void Shift(this IEnumerable<Line> lines, int lineNumber, DateTime correctTime)
+        {
+            var line = lines.Where(l => l.Sequence == lineNumber).FirstOrDefault();
+            if(line == null)
+            {
+                throw new Exception($"line number [{lineNumber}] not found in subtitles");
+            } else
+            {
+                Console.WriteLine($"Timestamp of line {lineNumber} in file is {line.StartTime:HH:mm:ss,fff} ");
+                int difference = (int)(correctTime - line.StartTime).TotalMilliseconds;
+                if(difference == 0)
+                {
+                    Console.WriteLine("No shifting required");
+                }
+                else
+                {
+                    Console.WriteLine($"Shifting all lines by {difference} milliseconds");
+                    lines.Shift(difference);
+                }
+            }
+        }
+        public static void Sync(this IEnumerable<Line> lines, int startLine, int endLine, DateTime correctEndTime)
+        {
+            DateTime startTimeInFile = lines.Where(l => l.Sequence == startLine).First().StartTime;
+            DateTime endTimeInFile = lines.Where(l => l.Sequence == endLine).First().StartTime;
+            var before = lines.Last().StartTime;
+
+            foreach(Line line in lines)
+            {
+                line.Sync(startTimeInFile, endTimeInFile, startTimeInFile, correctEndTime);
+            }
+
+            var after = lines.Last().StartTime;
+
+            int difference = (int)(after - before).TotalSeconds;
+            if(difference == 0) Console.WriteLine("No change noticed");
+            else Console.WriteLine($"subsync adjusted file to movie difference of [{difference}] seconds.");
+
+        }
+
+        public static string Find(this string[] args, string field, string defaultValue)
+        {
+            for(int i = 0; i<args.Length - 1; i++)
+            {
+                if(args[i] == field)
+                {
+                    return args[i + 1];
+                }
+            }
+            return defaultValue;
+        }
+        public static string Find(this string[] args, string field, int offset = 1)
+        {
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                if (args[i] == field)
+                {
+                    if((i+offset)<args.Length)
+                    {
+                        return args[i + offset];
+                    }
+                    else
+                    {
+                        throw new Exception($"offset [{i + offset}] exceeds arguments length of ({args.Length})");
+                    }
+                }
+            }
+            throw new Exception($"field ({field}) not found");
+        }
+
+        public static DateTime ToDateTime(this string text)
+        {
+            string[] times = text.Split(",");
+            return DateTime.Parse(times[0]).AddMilliseconds(int.Parse(times[1]));
+
+        }
+
+        public static bool IsDateTime(this string text)
+        {
+            return DateTime.TryParse(text, out _);
+        }
+
+        public static bool IsTimeStamp(this string text)
+        {
+            string[] parts = text.Split(",");
+            if(parts.Length == 2)
+            {
+                return parts[0].IsDateTime() && parts[1].IsMilliseconds();
+            }
+            return false;
+        }
+
+        public static bool IsMilliseconds(this string text)
+        {
+            if(text.Length == 3)
+            {
+                return int.TryParse(text, out _);
+            }
+            return false;
+        }
+
+        public static bool IsPositiveInteger(this string text)
+        {
+            int val;
+            if( int.TryParse(text, out val))
+            {
+                return val > 0;
+            }
+            return false;
+        }
+
+        public static bool IsInteger(this string text)
+        {
+
+            return int.TryParse(text, out _);
+
+        }
+
+        public static bool IsEmpty(this string text)
+        {
+            return string.IsNullOrEmpty(text);
+        }
+
+
+    }
+}
---
+++ SubSync/Program.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SubSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool fileFlag = false;
+            bool commandFlag = false;
+            bool valuesFlag = false;
+            bool outExists = false;
+            bool outputFlag = false;
+
+            if (args.Length == 3)
+            {
+                fileFlag = CheckInputFile(args[0]);
+                commandFlag = CheckCommand(args[1]);
+                valuesFlag = CheckValues(args[1], args[2]);
+                outputFlag = true;
+            }
+            else if (args.Length == 4)
+            {
+                fileFlag = CheckInputFile(args[0]);
+                commandFlag = CheckCommand(args[1]);
+                valuesFlag = CheckValues(args[1], args[2], args[3]);
+                outExists = CheckIfOutExists(args[2], args[3]);
+                if (!outExists)
+                {
+                    outputFlag = true;
+                }
+                
+            }
+            else if(args.Length == 5)
+            {
+                outExists = CheckIfOutExists(args[3], args[4]);
+                if(outExists)
+                {
+                    fileFlag = CheckInputFile(args[0]);
+                    commandFlag = CheckCommand(args[1]);
+                    valuesFlag = CheckValues(args[1], args[2]);
+                } else
+                {
+                    fileFlag = CheckInputFile(args[0]);
+                    commandFlag = CheckCommand(args[1]);
+                    valuesFlag = CheckValues(args[1], args[2], args[3], args[4]);
+                }
+                outputFlag = true;
+            }
+            else if (args.Length == 6)
+            {
+                fileFlag = CheckInputFile(args[0]);
+                commandFlag = CheckCommand(args[1]);
+                outExists = CheckIfOutExists(args[4], args[5]);
+                if (outExists)
+                {
+
+                    valuesFlag = CheckValues(args[1], args[2], args[3]);
+                    outputFlag = true;
+                }
+                else
+                {
+                    outputFlag = false;
+                }
+            }
+            else if(args.Length == 7)
+            {
+                outputFlag = CheckIfOutExists(args[5], args[6]);
+                if(outputFlag)
+                {
+                    fileFlag = CheckInputFile(args[0]);
+                    commandFlag = CheckCommand(args[1]);
+                    valuesFlag = CheckValues(args[1], args[2], args[3], args[4]);
+                }
+            }
+            else
+            {
+                PrintHelp();
+                return;
+            }
+
+            if(!fileFlag || !commandFlag || !valuesFlag || !outputFlag)
+            {
+                PrintHelp();
+                return;
+            }
+
+
+            string inputFile = args[0];
+
+            string outputFile = args.Find("out", args[0]);
+            List<Line> lines;
+            try
+            {
+                lines = ReadFile(inputFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while reading input file: {ex.Message}");
+                return;
+            }
+
+            
+
+            string command = args[1];
+            switch(command)
+            {
+                case "shift":
+                    if(args.Length == 3 || args.Length == 5)
+                    {
+                        int value = int.Parse(args.Find("shift"));
+                        lines.Shift(value);
+                    } else if (args.Length == 4 || args.Length == 6)
+                    {
+                        int lineNumber = int.Parse(args.Find("shift"));
+                        DateTime correctTime = args.Find("shift", 2).ToDateTime();
+                        try
+                        {
+                            lines.Shift(lineNumber, correctTime);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error: {ex.Message}");
+                            return;
+                        }
+                    }
+                    break;
+                case "sync":
+                    int startLine = int.Parse(args.Find("sync", 1));
+                    int endLine = int.Parse(args.Find("sync", 2));
+                    DateTime endTime = args.Find("sync", 3).ToDateTime();
+                    lines.Sync(startLine, endLine, endTime);
+                    break;
+                default:
+                    Console.WriteLine($"unrecognized command [{command}]");
+                    return;
+
+            }
+            
+
+
+            StringBuilder sb = new();
+            int cnt = 1;
+            foreach(Line line in lines)
+            {
+                sb.Append($"{cnt}\r\n");
+                sb.Append($"{line.Timing}\r\n");
+                sb.Append($"{line.Text}\r\n");
+                sb.AppendLine();
+                cnt += 1;
+            }
+
+
+            File.WriteAllText(outputFile, sb.ToString());
+
+#if DEBUG
+            //Process.Start("notepad", output);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+#endif
+        }
+
+        private static List<Line> ReadFile(string inputFile)
+        {
+            string sourceText;
+            bool IsSignatureFound = false;
+            bool IsSignInserted = false;
+
+            try
+            {
+                sourceText = File.ReadAllText(inputFile);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Unable to access input file [{inputFile}]: {ex.Message}");
+            }
+
+            
+
+            List<Line> lines = new();
+            var regex = new Regex(@"(?<sequence>\d+)\r\n(?<start_time>\d\d:\d\d:\d\d,\d\d\d) --\> (?<end_time>\d\d:\d\d:\d\d,\d\d\d)\r\n(?<text>[\s\S]*?\r\n\r\n)", RegexOptions.Compiled | RegexOptions.ECMAScript);
+            var sign = new Regex("subsync", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var signMatch = sign.Match(sourceText);
+            if(signMatch.Success) { IsSignatureFound = true; }
+
+            var matches = regex.Matches(sourceText);
+            int cnt = 0;
+
+
+            foreach (Match match in matches)
+            {
+                string sequence = match.Groups["sequence"].Value;
+                string startTime = match.Groups["start_time"].Value;
+                string endTime = match.Groups["end_time"].Value;
+
+                string text = match.Groups["text"].Value;
+                lines.Add(new Line(sequence, startTime.ToDateTime(), endTime.ToDateTime(), text));
+                
+                cnt += 1;
+                if(IsSignatureFound == false)
+                {
+                    if (cnt == matches.Count / 2 || cnt == matches.Count)
+                    {
+                        lines.Add(new Line(sequence + 111, startTime.ToDateTime().AddSeconds(2), endTime.ToDateTime().AddSeconds(4),
+                            "subsynced by subsync@abunoor.com"));
+                        IsSignInserted = true;
+                    }
+                }
+            }
+            if(IsSignInserted) Console.WriteLine("subsync signature inserted");
+            return lines;
+
+        }
+
+        private static bool CheckIfOutExists(string v1, string v2)
+        {
+            if(v1 == "out")
+            {
+                if(!string.IsNullOrEmpty(v2)) return true;
+                else return false;
+            }
+            return false;
+            
+        }
+
+        private static bool CheckInputFile(string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine($"input file [{path}] does not exist.");
+                    return false;
+                } else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error trying to check input file [{path}]: {ex.Message}");
+                return false;
+            }
+        }
+
+        private static bool CheckCommand(string comm)
+        {
+            if (comm == "shift" || comm == "sync") return true;
+            Console.WriteLine($"Invalid command [{comm}]");
+            return false;
+        }
+
+        private static bool CheckValues(string command, string v1, string v2 = null, string v3 = null)
+        {
+            bool ok1 = false;
+            bool ok2 = false;
+            bool ok3 = false;
+
+            if(command == "sync")
+            {
+                ok1 = v1.IsPositiveInteger();
+                if(!ok1)
+                {
+                    Console.WriteLine($"starting reference line [{v1}] must be a valid integer");
+                }
+                if (v2.IsEmpty()) Console.WriteLine("you must provide a reference line number for sync1");
+                else
+                {
+                    ok2 = v2.IsPositiveInteger();
+                    if (!ok2) Console.WriteLine($"line number [{v2}] must be a valid positive integer");
+                }
+                if (v3.IsEmpty()) Console.WriteLine("sync requires timestamp for reference line");
+                else
+                {
+                    ok3 = v3.IsTimeStamp();
+                    if(!ok3)
+                    {
+                        Console.WriteLine($"timestamp [{v3}] must be valid with format HH:mm:ss,fff");
+                    }
+                }
+
+            } else if (command == "shift")
+            {
+                ok3 = v3.IsEmpty();
+
+                if(v2.IsEmpty())
+                {
+                    ok1 = v1.IsInteger();
+                    ok2 = true;
+                    
+                    if(!ok1)
+                    {
+                        Console.WriteLine($"shift value [{v1}] must be a valid integer");
+                    }
+                } else
+                {
+                    ok1 = v1.IsPositiveInteger();
+                    ok2 = v2.IsTimeStamp();
+                    if(!ok1)
+                    {
+                        Console.WriteLine($"line number [{v1}] must be a valid positive integer");
+                    }
+                    if(!ok2)
+                    {
+                        Console.WriteLine($"timestamp [{v2}] must be valid with format HH:mm:ss,fff");
+                    }
+                }
+            }
+            
+            
+
+            return ok1 && ok2 && ok3;
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Usage:");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("    subsync inputFile <command> <value(s)> [out outputFile]");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Examples:  ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("    subsync the_village.srt shift 1300");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("       shift timing in each subtitles lines by adding 1300 milliseconds, and overwrite original file");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("    subsync the_village.srt shift 5 00:02:24,415");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("       the 5th line in the subtitles file should be on screen at the timestamp 00:02:24,415.");
+            Console.WriteLine("       all other lines are shifted according to the found time span difference");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("    subsync the_village.srt shift -26000 out the_village_fixed.srt");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("       shift timing in each subtitles lines by subtracting 26000 milliseconds, and save to the_village_fixed.srt");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("    subsync the_village.srt sync 5 643 01:14:23,015");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("       the 5th line is a reference time and is correct both in file and in movie");
+            Console.WriteLine("       the 643rd line is a out of sync and should be at the timestamp 01:14:23,015");
+            Console.WriteLine("       all other lines will get decrement/increment ratio based on the reference times");
+            Console.WriteLine();
+        }
+    }
+
+
+}
